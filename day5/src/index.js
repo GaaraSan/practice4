54f7c4971d276e43453c1b5ce974f63362d6496e
GaaraSan <prosto.artem158@gmail.com>
2023-01-12 22:30:28 +0200

    day 5 1/2

---
+++ day5/src/index.js
@@ -0,0 +1,31 @@
+import { createApp } from 'vue'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+ 
+createApp({
+    data() {
+      return {
+        students: [],
+        search: '',
+        student: { pib: '', zdav: false, group: '' },
+      };
+    },
+    mounted: function () {
+      axios.get('http://34.82.81.113:3000/students').then((response) => {
+        console.log(response.data);
+        this.students = response.data;
+      });
+      // this.students = students;
+    },
+    methods: {
+      deleteStudent(studId) {
+        this.students = this.students.filter((elem) => {
+          return elem.id != studId;
+        });
+      },
+      addStudent() {
+        this.student.id = this.students.length + 1;
+        this.students.push(this.student);
+      },
+    },
+  }).mount('#app');
