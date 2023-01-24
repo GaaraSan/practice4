1b2ebe19b0cb7fd0c766b5b08b035a744950fb65
GaaraSan <prosto.artem158@gmail.com>
2023-01-24 15:48:39 +0200

    add theme switch

--- day11/students/src/components/Students.vue
+++ day11/students/src/components/Students.vue
@@ -6,6 +6,7 @@ import axios from 'axios'
 const getStudentsApi = reactive({ students: [] })
 const store = useStore()
 let showEdit = false
+const isWhite = ref("")
 
 const getStudents = () => {
 	return axios.get('http://34.82.81.113:3000/students')
@@ -71,100 +72,124 @@ function editStudent(id) {
 	}
 }
 
+const changeTheme = function(){
+	isWhite.value = !isWhite.value
+	store.commit('changeTheme', isWhite.value);
+}
 </script>
 
 <template>
-	<div class="studentCount">
-		<h1>Всього студентів: {{ countStudents }}</h1>
-	</div>
-    <div :class="$style.task">
-    <input
-      type="text"
-      v-model="search"
-	  />
-    <div :class="$style.parent" v-for="student in getStudentsApi.students">
-		<div :class="$style.div1">
-			<router-link :class="[$style.nameStudent, search !== '' && `${student.name}`.indexOf(search) >= 0 && $style.divColor1]" :to="'/student-info/'+student._id">
-				{{ `${student.name}`}}
-			</router-link>
+	<div class="ustal" :class="{ white: isWhite , dark: !isWhite}">
+		<div class="studentCount">
+			<h1>Всього студентів: {{ countStudents }}</h1>
 		</div>
-      <div :class="$style.div2">{{ student.group }}</div>
-      <div :class="$style.div3">
-        <input
-          type="checkbox"
-          v-model="student.isDonePr"
-        >
-      </div>
-      <div :class="$style.div4"><a :class="$style.delete" @click="onClick(student._id)">Видалити</a></div>
-	  <div :class="$style.div5">
-        <img :class="$style.edit" :src="`../assets/edit.png`" alt="edit" @click="edit(student)">
-      </div>
-    </div>
-  </div>
-  <form
-		:class="$style.addStudent"
-		@submit.prevent="addStudent"
-	>
-		<label :class="$style.addStudentLabel">
-			ФІО
+		<div :class="$style.task">
+		<input
+		type="text"
+		v-model="search"
+		/>
+		<button @click="changeTheme"> Змінити тему</button>
+		<div :class="$style.parent" v-for="student in getStudentsApi.students">
+			<div :class="$style.div1">
+				<router-link :class="[$style.nameStudent, search !== '' && `${student.name}`.indexOf(search) >= 0 && $style.divColor1]" :to="'/student-info/'+student._id">
+					{{ `${student.name}`}}
+				</router-link>
+			</div>
+		<div :class="$style.div2">{{ student.group }}</div>
+		<div :class="$style.div3">
 			<input
-				type="text"
-				v-model="student.name"
-			/>
-		</label>
-
-		<label :class="$style.addStudentLabel">
-			Група
-			<input
-				type="text"
-				v-model="student.group"
-			/>
-		</label>
-
-		<label :class="$style.addStudentLabel">
-			Виконання роботи
-			<input
-				v-model="student.isDonePr"
-				type="checkbox"
-			/>
-		</label>
-
-		<button :class="$style.addStudentButton">Додати</button>
-	</form>
-	<hr>
-
-<!--  -->
+			type="checkbox"
+			v-model="student.isDonePr"
+			>
+		</div>
+		<div :class="$style.div4"><a :class="$style.delete" @click="onClick(student._id)">Видалити</a></div>
+		<div :class="$style.div5">
+			<img :class="$style.edit" :src="`../assets/edit.png`" alt="edit" @click="edit(student)">
+		</div>
+		</div>
+	</div>
 	<form
-		:class="$style.addStudent"
-		@submit.prevent="editStudent(editedStudent._id)"
-		v-show="showEdit"
-	>
-		<label :class="$style.addStudentLabel">
-			ФІО
-			<input
-				type="text"
-				v-model="editedStudent.name"
-			/>
-		</label>
+			:class="$style.addStudent"
+			@submit.prevent="addStudent"
+		>
+			<label :class="$style.addStudentLabel">
+				ФІО
+				<input
+					type="text"
+					v-model="student.name"
+				/>
+			</label>
 
-		<label :class="$style.addStudentLabel">
-			Група
-			<input
-				type="text"
-				v-model="editedStudent.group"
-			/>
-		</label>
+			<label :class="$style.addStudentLabel">
+				Група
+				<input
+					type="text"
+					v-model="student.group"
+				/>
+			</label>
 
-		<label :class="$style.addStudentLabel">
-			Виконання роботи
-			<input
-				v-model="editedStudent.isDonePr"
-				type="checkbox"
-			/>
-		</label>
+			<label :class="$style.addStudentLabel">
+				Виконання роботи
+				<input
+					v-model="student.isDonePr"
+					type="checkbox"
+				/>
+			</label>
 
-		<button :class="$style.addStudentButton">Редагувати</button>
-	</form>
+			<button :class="$style.addStudentButton">Додати</button>
+		</form>
+		<hr>
+
+	<!--  -->
+		<form
+			:class="$style.addStudent"
+			@submit.prevent="editStudent(editedStudent._id)"
+			v-show="showEdit"
+		>
+			<label :class="$style.addStudentLabel">
+				ФІО
+				<input
+					type="text"
+					v-model="editedStudent.name"
+				/>
+			</label>
+
+			<label :class="$style.addStudentLabel">
+				Група
+				<input
+					type="text"
+					v-model="editedStudent.group"
+				/>
+			</label>
+
+			<label :class="$style.addStudentLabel">
+				Виконання роботи
+				<input
+					v-model="editedStudent.isDonePr"
+					type="checkbox"
+				/>
+			</label>
+
+			<button :class="$style.addStudentButton">Редагувати</button>
+		</form>
+	</div>
 </template>
 
 <style src="../assets/style.css" lang="css" module></style>
+<style>
+.ustal{
+	height: 100%;
+	width: 100%;
+	position: absolute;
+	top: 0;
+	left: 0;
+	padding: 30px;
+}
+.dark{
+   background-color: rgb(65, 65, 65);
+   color:white;
+}
+.white{
+   background-color: #fff;
+}
+</style>
--- day11/students/src/store.js
+++ day11/students/src/store.js
@@ -1,18 +1,25 @@
 import { createStore } from 'vuex'
-
+import createPersistedState from "vuex-persistedstate";
 
 const store = createStore({
+    plugins: [createPersistedState()],
     state: {
-       count: 0
-     },
-     mutations: {
-         setCount: (state, count) => state.count = count,
-     },
-     getters: {
-         getCount: (state) =>
-         {
-             return state.count
-         }
-     }  
- })
+        count: 0,
+        isWhite: true
+    },
+    mutations: {
+        setCount: (state, count) => state.count = count,
+        changeTheme: (state, isWhite) => state.isWhite = isWhite,
+    },
+    getters: {
+        getCount: (state) =>
+        {
+            return state.count
+        },
+        getTheme: (state) =>
+        {
+            return state.isWhite
+        }
+    }  
+})
 export default store;
